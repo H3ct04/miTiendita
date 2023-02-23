ae1c4505f80ad705d0bd955708473498d4e0ffa8
Hector <hector.carrasco.jobs@gmail.com>
2023-02-23 14:15:43 -0300

    primer commit ahora si

--- src/main/java/cl/ds/app/App.java
+++ src/main/java/cl/ds/app/App.java
@@ -3,5 +3,6 @@ package main.java.cl.ds.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("cambios");
     }
 }
